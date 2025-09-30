85d43ae55a21c9d0f160c11c449766b865eab090
ElsaDwiAgresty <165319077+ElsaDwiAgresty@users.noreply.github.com>
2025-09-30 19:45:20 +0700

    commit pertama dsp

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+@app.route('/dashboard')
+def get_dashboard():
+    return render_template('dashboard_view.html')
+
+import os
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
